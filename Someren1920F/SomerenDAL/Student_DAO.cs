d03ddca338a68dd383a3fa8518d0e81474bafe8b
Harks, Sander <650397@student.inholland.nl>
2021-02-28 17:23:00 +0100

    added original solution

---
+++ Someren1920F/SomerenDAL/Student_DAO.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data;
+using System.Collections.ObjectModel;
+using SomerenModel;
+
+namespace SomerenDAL
+{
+    public class Student_DAO : Base
+    {
+      
+        public List<Student> Db_Get_All_Students()
+        {
+            string query = "SELECT student_id, student_name FROM [TABLE]";
+            SqlParameter[] sqlParameters = new SqlParameter[0];
+            return ReadTables(ExecuteSelectQuery(query, sqlParameters));
+        }
+
+        private List<Student> ReadTables(DataTable dataTable)
+        {
+            List<Student> students = new List<Student>();
+
+            foreach (DataRow dr in dataTable.Rows)
+            {
+                Student student = new Student()
+                {
+                    Number = (int)dr["student_id"],
+                    Name = (String)(dr["student_name"].ToString())
+                };
+                students.Add(student);
+            }
+            return students;
+        }
+
+    }
+}
---
+++ Someren1920F/SomerenModel/Student.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SomerenModel
+{
+    public class Student
+    { 
+        public String Name { get; set; }
+        public int Number { get; set; } // StudentNumber, e.g. 474791
+        public DateTime BirthDate { get; set;  }
+
+    }
+}
